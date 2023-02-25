868ccddb80e6aa2122ec009d4e5161c30d084d51
Jothish JJ <jothishjj11@gmail.com>
2023-02-25 12:12:59 +0000

    Make everything work

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import * as Speech from "expo-speech";
 
 export default function App() {
+  function start() {
+    setInterval(() => {
+      let odds = Math.random();
+
+      if (odds < 0.5) {
+        Speech.speak("right", {
+          language: "pt-BR",
+        });
+      } else {
+        Speech.speak("left", {
+          language: "pt-BR",
+        });
+      }
+    }, 5000);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Tap to Start saying!</Text>
+      <Button title="Start" onPress={start} />
     </View>
   );
 }
@@ -13,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
